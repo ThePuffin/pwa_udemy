d2aa2bc16315b82dee661a342966e11c25fe50e9
Vinz <ballutvincent@gmail.com>
2020-12-13 09:41:02 +0100

    add a static and a dynamic cache for offline request

--- sw.js
+++ sw.js
@@ -1,14 +1,44 @@
+const staticCache = "static-cache";
+const dynamicCache = "dynamic-cache";
+const assets = [
+  "/",
+  "/index.html",
+  "/js/ui.js",
+  "/js/app.js",
+  "/js/materialize.min.js",
+  "/css/materialize.min.css",
+  "/css",
+  "/manif.json",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+];
+
 //install process
-self.addEventListener('install', (e) => {
-    console.log('sw is installed');
-})
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(staticCache).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
 
 //activate process
-self.addEventListener('activate', (e) => {
-    console.log('sw is activated');
-})
+self.addEventListener("activate", (e) => {
+  console.log("sw is activated");
+});
 
 //fecth events
-self.addEventListener('fetch', (e) => {
-    console.log('fetch event',e);
-})
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((staticRes) => {
+      return (
+        staticRes ||
+        fetch(e.request).then((dynamicRes) => {
+          return caches.open(dynamicCache).then((cache) => {
+            cache.put(e.request.url, dynamicRes.clone());
+            return dynamicRes;
+          });
+        })
+      );
+    })
+  );
+});
